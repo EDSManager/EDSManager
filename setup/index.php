d39b6187a3245206853888c87ede9aa683a1d9d8
FEW172 <few_172@mail.ru>
2021-05-17 14:51:08 +0300

    Update

--- setup/index.php
+++ setup/index.php
@@ -2,5 +2,17 @@
 // SETUP PAGE
 
 echo 'Текущая версия PHP: ' . phpversion();
+echo ' - ';
 
+$ver = (float)phpversion();
+
+if ($ver > 7.0) {
+    echo 'Ok';
+
+} elseif ($ver === 7.0) {
+    echo 'Ok';
+
+} else {
+    echo 'Error. Версия PHP не поддерживается';
+}
 ?>
