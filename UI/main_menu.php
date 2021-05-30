6a2e05430c671bc43f5ffa1f648fcfc789d2faed
FEW172 <few_172@mail.ru>
2021-05-30 17:23:25 +0300

    Update

--- UI/main_menu.php
+++ UI/main_menu.php
@@ -5,16 +5,24 @@
 
 if (isset ($_SESSION["userid"])) {
 
-echo '<nav class="menu_color">';
-echo '<ul class="my_menu">';
-echo '    <li><a href="/">пункт 1</a></li>';
-echo '    <li><a href="/">пунтк 2</a></li>';
-echo '    <li><a href="/">пункт 3</a></li>';
-echo '    <li><a href="/">пункт 4</a></li>';
-echo '    <li><a href="logoff.php">Выход</a></li>';
-echo '  </ul>';
-echo '</nav>';
+    echo "<!DOCTYPE html>";
+    echo "<head><title>EDSManager</title>";
+    echo '<meta charset="utf-8">';
+    echo '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
+    echo '<link rel="stylesheet" href="../css/style.css">';
+    echo "</head>";
 
+    echo "<body>";
+
+echo '    <div id="menu">';
+echo '    <p>	<a href="#">ссылка 1</a> </p>';
+echo '    <p>	<a href="#">ссылка 2</a> </p>';
+echo '    <p>	<a href="#">ссылка 3</a> </p>';
+echo '    <p>	<a href="#">ссылка 4</a> </p>';
+echo '    <p>	<a href="logoff.php">Выход</a> </p>';
+echo '    </div> ';
+
+echo '</body>';
 }
 else {
 
