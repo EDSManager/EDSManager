7efcb93c91f0bb0550e62b72217e93b2042a2458
FEW172 <few_172@mail.ru>
2021-06-07 09:27:33 +0300

    Update

--- UI/admin.php
+++ UI/admin.php
@@ -8,13 +8,37 @@ session_start();
 if (isset ($_SESSION["userid"])) {
 
 
-    echo "Добро пожаловать в EDSManager, ";
+    echo '<!DOCTYPE html>';
+    echo '<html lang="ru">';
+    echo '<head><title>EDS Manager</title>';
+    echo '<meta charset="utf-8" />';
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0" />';
+    echo '<link rel="stylesheet" href="../css/style.css">';
 
-    echo $_SESSION["userid"];
+    echo '</head>';
+    echo '<body>';
+    echo '<div id="headerInner">';
+    echo '  <div class="logo">';
+    echo '     <a href="#">EDS Manager</a>';
+    echo '  </div>';
 
-    include("./main_menu.php");
+    echo '<div class = "sprava">               ';
+    echo "Добро пожаловать в EDSManager, ".$_SESSION["userid"];
+    echo '</div>';
 
-    echo "Пользователи:";
+    echo '</div>';
+    echo '<!-- начало wrapper -->';
+    echo '<div id="wrapper">';
+    echo '   <div id="middle">';
+    echo '       <div id="content">';
+
+    include ("./main_menu.php");
+
+    echo '          <div id="colMain">';
+    echo '              <div class="text">';
+
+
+    echo "<h1>Пользователи:</h1>";
 
 // подключаемся к серверу с параметрами из конфигурационного файла
     $link = mysqli_connect($MySettings['db_host'], $MySettings['db_user'], $MySettings['db_pwd'], $MySettings['db_name']);
@@ -42,6 +66,15 @@ if (isset ($_SESSION["userid"])) {
     mysqli_close($link);
 
     echo '<a href="./add_user.php"> Добавить пользователя</a>';
+
+
+    echo '              </div>';
+    echo '          </div><!-- конец colLeft -->';
+    echo '       </div><!-- конец content -->';
+    echo '   </div><!-- конец middle -->';
+    echo '</div><!-- конец wrapper -->';
+    echo '</body>';
+    echo '</html>';
 
 }
 else {
