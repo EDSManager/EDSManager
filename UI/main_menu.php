f1a3f03b12453e79cd0a655c48745de19d203c33
FEW172 <few_172@mail.ru>
2021-06-07 14:41:54 +0300

    Update

--- UI/main_menu.php
+++ UI/main_menu.php
@@ -12,7 +12,7 @@ echo ' <div class="colMenu">';
 echo ' <p>	<a href="#">ссылка 1</a> </p>';
 echo ' <p>	<a href="#">ссылка 2</a> </p>';
 echo ' <p>	<a href="#">ссылка 3</a> </p>';
-echo ' <p>	<a href="admin.php">Администрирование</a> </p>';
+echo ' <p>	<a href="admin.php">Инструменты администратора</a> </p>';
 echo ' <p>	<a href="logoff.php">Выход</a> </p>';
 echo ' </div>';
 echo '</div>';
