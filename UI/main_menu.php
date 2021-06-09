236b8c02c6967ae792dec33a2fc64ab52f89a086
FEW172 <few_172@mail.ru>
2021-06-09 17:36:49 +0300

    Update

--- UI/main_menu.php
+++ UI/main_menu.php
@@ -11,7 +11,7 @@ if (isset ($_SESSION["userid"])) {
     echo '<div class="colMenu">';
     echo '<p><a href="#">ссылка 1</a> </p>';
     echo '<p><a href="#">ссылка 2</a> </p>';
-    echo '<p><a href="#">ссылка 3</a> </p>';
+    echo '<p><a href="../setup/mysql_db_update.php">Обновить БД</a></p>';
     echo '<p><a href="admin.php">Инструменты администратора</a> </p>';
     echo '<p><a href="logoff.php">Выход</a> </p>';
     echo '</div>';
