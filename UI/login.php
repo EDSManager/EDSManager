2a4e43b32972410d156cd7983d919d3ed99bf562
Дмитрий <vershansky1990@gmail.com>
2021-06-09 16:03:49 +0300

    09.06.2021 Update

--- UI/login.php
+++ UI/login.php
@@ -16,39 +16,38 @@ if (!empty($_POST)) {
 ?>
 
 <!DOCTYPE html>
-<html lang="ru">
-<head><title>Вход в EDSManager</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <html lang="ru">
+        <head>
+            <title>Вход в EDSManager</title>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+            <link rel="stylesheet" href="../css/style.css">
+        </head>
 
-<link rel="stylesheet" href="../css/style.css">
-</head>
-
-<body>
-<section class="container">
-
-<div id="headerInner">
-<div class="logo">
-<a href="/">EDS Manager</a>
-</div>
-</div>
+    <body>
+        <section class="container">
+            <div id="headerInner">
+                <div class="logo">
+                    <a href="/">EDS Manager</a>
+                </div>
+            </div>
 
 <div class="login">
+
 <h1>EDSManager</h1>
+                <form action="./login.php" method="post" enctype="multipart/form-data">
+                    <p><label><input type="text" name="q_login" value="" placeholder="Логин"></label></p>
+                    <p><label><input type="password" name="q_password" value="" placeholder="Пароль"></label></p>
 
-<form action="./login.php" method="post" enctype="multipart/form-data">
-<p><label><input type="text" name="q_login" value="" placeholder="Логин"></label></p>
-<p><label><input type="password" name="q_password" value="" placeholder="Пароль"></label></p>
+                        <?php if (isset($error)):
+                            echo '<span style="color: red;">';
+                            echo $error;
+                            echo '</span>';
+                        endif; ?>
 
-        <?php if (isset($error)):
-            echo '<span style="color: red;">';
-            echo $error;
-            echo '</span>';
-        endif; ?>
-
-<p class="submit"><input type="submit" name="commit" value="Войти"></p>
-</form>
-</div>
-</section>
-</body>
+                    <p class="submit"><input type="submit" name="commit" value="Войти"></p>
+                </form>
+            </div>
+        </section>
+    </body>
 </html>
