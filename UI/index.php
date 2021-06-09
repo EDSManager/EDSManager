6f126e6252b88fa191c2c4fa9da1b43ec11ff45c
FEW172 <few_172@mail.ru>
2021-06-09 18:44:47 +0300

    Update

--- UI/index.php
+++ UI/index.php
@@ -6,35 +6,4 @@ if (isset ($_SESSION["userid"])) {
 
     header("Location: ./main.php");
 
-} else {
-
-    echo "<!DOCTYPE html>";
-    echo "<head><title>Вход в EDSManager</title>";
-    echo '<meta charset="utf-8">';
-    echo '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
-
-    echo '<link rel="stylesheet" href="../css/style.css">';
-    echo "</head>";
-
-    echo "<body>";
-    echo '<section class="container">';
-
-    echo '<div id="headerInner">';
-    echo '<div class="logo">';
-    echo '<a href="/">EDS Manager</a>';
-    echo '</div>';
-    echo '</div>';
-
-    echo '<div class="login">';
-    echo "<h1>EDSManager</h1>";
-    echo '<form action="check_user.php" method="post" enctype="multipart/form-data">';
-    echo '<p><input type="text" name="q_login" value="" placeholder="Логин или Email"></p>';
-    echo '<p><input type="password" name="q_password" value="" placeholder="Пароль"></p>';
-    echo '<p class="submit"><input type="submit" name="commit" value="Войти"></p>';
-    echo "</form>";
-    echo "</div>";
-    echo "</section>";
-    echo "</body>";
-    echo "</html>";
-
-};
+} else header("Location: ./login.php");;
--- UI/login.php
+++ UI/login.php
@@ -1,36 +1,65 @@
 <?php
+
+require_once ('../approot.inc.php');
+require_once (CONFIG_FILE);
+
 if (!empty($_POST)) {
 
-    $login = $_POST['q_login'];
-    $password = $_POST['q_password'];
+    $sQLogin = $_POST['q_login'];
+    $sQPassword = $_POST['q_password'];
 
-    $error = '';
+    $sError = '';
 
-    if (!preg_match("#^[aA-zZ0-9]+$#",$login)) {
-        $error = 'В логине недопустимые символы';
-    } else if ($password == '') {
-        $error = 'Пароль не может быть пустым';
-    } else if ($login != 'eds') {
-        $error = 'Неверный логин или пароль';
-    };
+    if (!preg_match("#^[aA-zZ0-9]+$#",$sQLogin)) {
+        $sError = 'В логине недопустимые символы';
+    } else if ($sQPassword == '') {
+        $sError = 'Пароль не может быть пустым';
+    }
 
-    echo date ('m.d.y H:i:s');
-    echo "<br>";
-    echo "User: ".$login;
-    echo "<br>";
-    echo "Браузер: ".$_SERVER['HTTP_USER_AGENT'];
-    echo "<br>";
-    echo "Remote address: ".$_SERVER['REMOTE_ADDR'];
-    echo "<br>";
-    echo "Status: ";
+    if (empty($sError)) {
 
-    if ($error == '') {
-        echo "OK";
-    } else {
-        echo "Error";
-    };
+        $bLink = mysqli_connect($MySettings['db_host'], $MySettings['db_user'], $MySettings['db_pwd'], $MySettings['db_name']);
+        if (!$bLink) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
 
+        $sQuery = "SELECT id FROM users WHERE login = '" . $sQLogin . "' AND password = '" . MD5($sQPassword) . "'";
 
+        $bResult = mysqli_query($bLink, $sQuery) or die("Connection failed: " . mysqli_connect_error());;
+        $aRow = mysqli_fetch_row($bResult);
+        $iUserId = (int)$aRow[0];
+
+        mysqli_close($bLink);
+
+        if ($iUserId > 0) {
+
+            session_start();
+            $_SESSION['userid'] = $iUserId;
+
+            header("Location: ./main.php");
+
+        } else {
+
+            $sError = "Неверный логин или пароль";
+        }
+
+    }
+
+//    echo date ('m.d.y H:i:s');
+//    echo "<br>";
+//    echo "User: ".$login;
+//    echo "<br>";
+//    echo "Браузер: ".$_SERVER['HTTP_USER_AGENT'];
+//    echo "<br>";
+//    echo "Remote address: ".$_SERVER['REMOTE_ADDR'];
+//    echo "<br>";
+//    echo "Status: ";
+
+//    if ($error == '') {
+//        echo "OK";
+//    } else {
+//        echo "Error";
+//    };
 };
 ?>
 
@@ -51,16 +80,17 @@ if (!empty($_POST)) {
                 </div>
             </div>
 
-<div class="login">
+            <div class="login">
 
-<h1>EDSManager</h1>
+                <h1>Вход в EDSManager</h1>
+
                 <form action="./login.php" method="post" enctype="multipart/form-data">
                     <p><label><input type="text" name="q_login" value="" placeholder="Логин"></label></p>
                     <p><label><input type="password" name="q_password" value="" placeholder="Пароль"></label></p>
 
-                        <?php if (isset($error)):
+                        <?php if (isset($sError)):
                             echo '<span style="color: red;">';
-                            echo $error;
+                            echo $sError;
                             echo '</span>';
                         endif; ?>
 
