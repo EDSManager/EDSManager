132e1d89151b88f9910a2b64d6e04247602435bb
FEW172 <few_172@mail.ru>
2021-06-28 09:15:09 +0300

    Disable UPDATE tables

--- UI/menu.inc.php
+++ UI/menu.inc.php
@@ -11,13 +11,36 @@
             </select>
         </div>
 
-        <div class="colMenu">
-            <p><a href="#">ссылка 1</a> </p>
-            <p><a href="#">ссылка 2</a> </p>
-            <p><a href="../setup/mysql_db_update.php">Обновить БД</a></p>
-            <p><a href="admin.php">Инструменты администратора</a> </p>
-            <p><a href="logoff.php">Выход</a> </p>
+    <section class="colMenu">
+        <div>
+            <input id="ac-1" name="accordion-1" type="radio" checked />
+            <label for="ac-1">Добро пожаловать</label>
+                <p><a href="..\">Основной контент</a></p>
+
         </div>
+        <div>
+            <input id="ac-2" name="accordion-1" type="radio" />
+            <label for="ac-2">Меню1</label>
+        </div>
+        <div>
+            <input id="ac-3" name="accordion-1" type="radio" />
+            <label for="ac-3">Меню2</label>
+        </div>
+        <div>
+            <input id="ac-4" name="accordion-1" type="radio" />
+            <label for="ac-4">Инструменты администратора</label>
+
+                <p><a href="../setup/mysql_db_update.php">Обновить БД</a></p>
+                <p><a href="admin.php">Инструменты администратора</a> </p>
+        </div>
+        <div>
+            <input id="ac-5" name="accordion-1" type="radio" />
+            <label for="ac-5">Выход</label>
+            <p><a href="logoff.php">Выход</a></p>
+
+        </div>
+    </section>
+
     </div>
     <!-- конец colMenu -->
 
